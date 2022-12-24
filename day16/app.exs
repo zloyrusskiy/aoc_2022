006b2607e76c14fbc61fa969a723d8de5e8a752b
Alexandr Fedorov <alexandrfedorov@gmail.com>
2022-12-24 12:32:20 +0400

    added day16 part 2 + refactored it's part 1

--- day16/app.exs
+++ day16/app.exs
@@ -1,52 +1,91 @@
 defmodule Valve do
-  defstruct [:label, :flow_rate, :neighbors, opened: false]
+  defstruct [:label, :flow_rate, :neighbors]
+end
+
+defmodule ValveGraph do
+  defstruct [:valve_map, :reach_map]
+end
+
+defmodule Traveller do
+  defstruct [:id, :rest_time, :cur_valve, :total_pressure]
 end
 
 defmodule Solution do
+  @open_valve_time 1
+  @part1_time 30
+  @part2_time 26
+  @start_valve "AA"
+
   def part1(input) do
-    valves = parse_valves(input)
+    valve_map = parse_valves(input)
+    reach_map = calc_distances_between_working_valves(Map.values(valve_map))
 
-    reach_map = calc_distances_between_working_valves(valves)
+    me = %Traveller{id: 1, rest_time: @part1_time, cur_valve: @start_valve, total_pressure: 0}
 
-    traverse_cave(reach_map, valves)
+    find_max_pressure([me], %ValveGraph{valve_map: valve_map, reach_map: reach_map})
   end
 
   def part2(input) do
-    :not_implemented
+    valve_map = parse_valves(input)
+    reach_map = calc_distances_between_working_valves(Map.values(valve_map))
+
+    me = %Traveller{id: 1, rest_time: @part2_time, cur_valve: @start_valve, total_pressure: 0}
+    elephant = %Traveller{id: 2, rest_time: @part2_time, cur_valve: @start_valve, total_pressure: 0}
+
+    find_max_pressure([me, elephant], %ValveGraph{valve_map: valve_map, reach_map: reach_map})
   end
 
-  defp traverse_cave(reach_map, valves) do
-    start_valve = Enum.find(valves, &(&1.label == "AA"))
-
-    working_valves = Enum.filter(valves, &(&1.flow_rate > 0))
-
-    do_traverse_cave(start_valve, working_valves, 0, 0, reach_map)
+  defp find_max_pressure(travellers, graph) do
+    graph.valve_map
+    |> Map.values()
+    |> Enum.filter(&(&1.flow_rate > 0))
+    |> Enum.map(&(&1.label))
+    |> do_find_max_pressure(travellers, graph)
   end
 
-  defp do_traverse_cave(_, _, cur_time, final_pressure, _) when cur_time > 30, do: final_pressure
-
-  defp do_traverse_cave(valve = %{opened: false, flow_rate: flow_rate}, rest_valves, cur_time, final_pressure, reach_map) when flow_rate > 0 do
-    do_traverse_cave(%{valve | opened: true}, rest_valves, cur_time + 1, final_pressure, reach_map)
+  defp do_find_max_pressure([], travellers, _) do
+    travellers |> Enum.map(&(&1.total_pressure)) |> Enum.sum
   end
 
-  defp do_traverse_cave(cur_valve, rest_valves, cur_time, final_pressure, reach_map) do
-    new_final_pressure = final_pressure + (30 - cur_time) * cur_valve.flow_rate
+  defp do_find_max_pressure(rest_valves, travellers, graph) do
+    with cur_traveller = %Traveller{} <- get_next_traveller(travellers)
+      do
+        Enum.map(rest_valves, fn next_valve ->
+          mod_traveller = go_to_next_valve(cur_traveller, next_valve, graph)
+          trav_id = mod_traveller.id
+          mod_travellers = travellers
+            |> Enum.map(fn
+              %{id: ^trav_id} -> mod_traveller
+              traveller -> traveller
+            end)
+          do_find_max_pressure(rest_valves -- [next_valve], mod_travellers, graph)
+        end)
+        |> Enum.max
+      else
+        :finish -> do_find_max_pressure([], travellers, graph)
+      end
+  end
 
-    case rest_valves do
-      [] -> new_final_pressure
-      _ -> rest_valves
-      |> Enum.map(fn valve ->
-        time_to_target = Map.get(reach_map, {cur_valve.label, valve.label})
+  defp get_next_traveller(travellers) do
+    travellers
+    |> Enum.filter(&(&1.rest_time > @open_valve_time))
+    |> Enum.max_by(&(&1.rest_time), &>=/2, fn -> :finish end)
+  end
 
-        do_traverse_cave(valve, List.delete(rest_valves, valve), cur_time + time_to_target, new_final_pressure, reach_map)
-      end)
-      |> Enum.max()
+  defp go_to_next_valve(trav, valve_label, graph) do
+    valve = Map.get(graph.valve_map, valve_label)
+    time_to_reach = Map.get(graph.reach_map, {trav.cur_valve, valve_label})
+    time_after = trav.rest_time - time_to_reach - @open_valve_time
+    final_pressure = time_after * valve.flow_rate
+    cond do
+      time_after > 0 -> %{trav | cur_valve: valve_label, rest_time: time_after, total_pressure: trav.total_pressure + final_pressure }
+      true -> %{trav | cur_valve: valve_label, rest_time: 0}
     end
   end
 
   defp calc_distances_between_working_valves(valves) do
     valves
-    |> Enum.filter(fn valve -> valve.label == "AA" or valve.flow_rate > 0 end)
+    |> Enum.filter(fn valve -> valve.label == @start_valve or valve.flow_rate > 0 end)
     |> Enum.reduce(Map.new(), fn valve, reach_map ->
       do_calc_distances_between_working_valves([valve], 0, valve, valves, MapSet.new(), reach_map)
     end)
@@ -100,8 +139,9 @@ defmodule Solution do
 
       neighbors = String.split(neighbors_labels, ", ")
 
-      %Valve{label: label, flow_rate: flow_rate, neighbors: neighbors}
+      {label, %Valve{label: label, flow_rate: flow_rate, neighbors: neighbors}}
     end)
+    |> Map.new()
   end
 end
 
